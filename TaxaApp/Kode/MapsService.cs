976e5c7ed7cab32e542b138cdebbabe960bfc7eb
David Frørup <dafr1@elev.tec.dk>
2024-02-12 12:22:31 +0100

    Version 2. 12-02-2024 12:22

--- TaxaApp/Kode/MapsService.cs
+++ TaxaApp/Kode/MapsService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -14,40 +13,38 @@ namespace TaxaApp.Kode
             _httpClient = httpClient;
         }
 
-        public async Task<int> GetDistanceInKm(string startAddress, string destinationAddress)
+        public async Task<int> GetDistanceIKm(string startAddress, string destinationAddress)
         {
             try
             {
-                // Erstat 'YOUR_API_KEY' med din faktiske Google Maps API-nøgle
-                string apiKey = "AIzaSyAMX2xaib8pawKfNoBlzSSFy5tCzo3RI7g";
+                string apiKey = "AIzaSyAMX2xaib8pawKfNoBlzSSFy5tCzo3RI7g"; // Erstat med din faktiske API-nøgle
                 string apiUrl = $"https://maps.googleapis.com/maps/api/distancematrix/json?origins={startAddress}&destinations={destinationAddress}&key={apiKey}";
 
-                var response = await _httpClient.GetAsync(apiUrl);
+                var response = await _httpClient.GetStringAsync(apiUrl);
 
-                if (response.IsSuccessStatusCode)
+                var distanceIndex = response.IndexOf("\"value\"");
+                if (distanceIndex != -1)
                 {
-                    using (var stream = await response.Content.ReadAsStreamAsync())
-                    using (var reader = new StreamReader(stream))
+                    distanceIndex = response.IndexOf(":", distanceIndex) + 1;
+                    var distanceEndIndex = response.IndexOf(",", distanceIndex);
+
+                    if (distanceEndIndex != -1 && int.TryParse(response.Substring(distanceIndex, distanceEndIndex - distanceIndex), out int distanceInMeters))
                     {
-                        string result = await reader.ReadToEndAsync(); // Ændring her
+                        int distanceInKm = distanceInMeters / 1000;
 
-                        // Implementer logikken til at trække afstanden fra JSON-svaret her
-                        // I dette eksempel antages det, at afstanden findes i kilometer
-                        // Du skal parse og behandle resultatet i overensstemmelse hermed
-                        Console.WriteLine(result);
-
-                        return 0; // Erstat denne linje med logikken til at hente den faktiske afstand
+                        Console.WriteLine($"Afstand: {distanceInKm} km");
+                        return distanceInKm;
                     }
                 }
-                else
-                {
-                    // Håndter fejl ved API-opkald her
-                    return 0;
-                }
+
+                Console.WriteLine("Fejl: Ugyldig afstandsinformation i svaret.");
+                Console.WriteLine("Svaret fra Google Maps API:");
+                Console.WriteLine(response);
+
+                return 0;
             }
             catch (Exception ex)
             {
-                // Håndter eventuelle undtagelser her
                 Console.WriteLine($"Fejl: {ex.Message}");
                 return 0;
             }
--- TaxaApp/Kode/PrisUdregner.cs
+++ TaxaApp/Kode/PrisUdregner.cs
@@ -4,62 +4,63 @@ namespace TaxaApp.Kode
 {
     public class PrisUdregner
     {
-        
-
-        public double Pris(Taxatur taxatur)
+        public class PrisInformation
         {
+            public double StartPris { get; set; }
+            public double KilometerPris { get; set; }
+            public double MinutPris { get; set; }
+            public double TotalPris { get; set; }
+        }
 
-            double StartPris = 0;
-            double KilometerPris = 0;
-            double MinutPris = 0;
+        public PrisInformation UdregnPris(Taxatur taxatur)
+        {
+            PrisInformation prisInfo = new PrisInformation();
 
             if (taxatur.VognType == "Almindelig vogn")
             {
                 if (taxatur.Tidspunkt == "Dag")
                 {
-                    StartPris = 37;
-                    KilometerPris = 12.75;
-                    MinutPris = 5.75;
+                    prisInfo.StartPris = 37;
+                    prisInfo.KilometerPris = 12.75;
+                    prisInfo.MinutPris = 5.75;
                 }
                 else
                 {
-                    StartPris = 47;
-                    KilometerPris = 16;
-                    MinutPris = 7;
+                    prisInfo.StartPris = 47;
+                    prisInfo.KilometerPris = 16;
+                    prisInfo.MinutPris = 7;
                 }
             }
             else if (taxatur.VognType == "Stor vogn")
             {
                 if (taxatur.Tidspunkt == "Dag")
                 {
-                    StartPris = 77;
-                    KilometerPris = 17;
-                    MinutPris = 5.75;
+                    prisInfo.StartPris = 77;
+                    prisInfo.KilometerPris = 17;
+                    prisInfo.MinutPris = 5.75;
                 }
                 else
                 {
-                    StartPris = 87;
-                    KilometerPris = 19;
-                    MinutPris = 7;
+                    prisInfo.StartPris = 87;
+                    prisInfo.KilometerPris = 19;
+                    prisInfo.MinutPris = 7;
                 }
             }
 
-            double TotalPris = StartPris + KilometerPris;
+            // Opdateret: Brug afstand fra taxatur
+            prisInfo.TotalPris = prisInfo.StartPris + (taxatur.DistanceIKm * prisInfo.KilometerPris);
 
             if (taxatur.HarCykel)
             {
-                TotalPris += 30;
+                prisInfo.TotalPris += 30;
             }
 
             if (taxatur.HarLiftvogn)
             {
-                TotalPris += 350;
+                prisInfo.TotalPris += 350;
             }
 
-            return TotalPris;
-
-
+            return prisInfo;
         }
-
     }
 }
